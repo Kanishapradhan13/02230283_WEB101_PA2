1183c28d13757bed626fb7d11b842c52d941447d
Kanisha13 <02230283.cst@rub.edu.bt>
2024-06-11 22:56:50 +0600

    pokedex

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,217 @@
-import Image from "next/image";
+"use client";
+import React, { useState, useEffect } from 'react';
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
-export default function Home() {
+const PokemonApp = () => {
+  const [query, setQuery] = useState('');
+  const [pokemons, setPokemons] = useState([]);
+  const [selectedPokemon, setSelectedPokemon] = useState(null);
+  const [speciesData, setSpeciesData] = useState(null);
+  const [caughtPokemons, setCaughtPokemons] = useState([]);
+  const [view, setView] = useState('search');
+
+  useEffect(() => {
+    const storedCaughtPokemons = JSON.parse(localStorage.getItem('caughtPokemons')) || [];
+    setCaughtPokemons(storedCaughtPokemons);
+  }, []);
+
+  const handleSearch = async () => {
+    if (query.trim()) {
+      try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${query.toLowerCase()}`);
+        if (response.ok) {
+          const data = await response.json();
+          setSelectedPokemon(data);
+          setPokemons([]); // Clear the list of the first 20 Pokémon when searching for a specific one
+          fetchSpeciesData(data.species.url);
+          addToCaught(data);
+        } else {
+          setSelectedPokemon(null);
+          alert('Pokemon not found!');
+        }
+      } catch (error) {
+        console.error('Error fetching the Pokémon:', error);
+      }
+    }
+  };
+
+  const handleClick = async (pokemonName) => {
+    try {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+      if (response.ok) {
+        const data = await response.json();
+        setSelectedPokemon(data);
+        fetchSpeciesData(data.species.url);
+        addToCaught(data);
+      } else {
+        alert('Pokemon not found!');
+      }
+    } catch (error) {
+      console.error('Error fetching the Pokémon:', error);
+    }
+  };
+
+  const fetchSpeciesData = async (url) => {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        setSpeciesData(data);
+      } else {
+        setSpeciesData(null);
+        alert('Species data not found!');
+      }
+    } catch (error) {
+      console.error('Error fetching the species data:', error);
+    }
+  };
+
+  const addToCaught = (pokemon) => {
+    const caughtPokemons = JSON.parse(localStorage.getItem('caughtPokemons')) || [];
+    if (!caughtPokemons.find(p => p.name === pokemon.name)) {
+      caughtPokemons.push(pokemon);
+      localStorage.setItem('caughtPokemons', JSON.stringify(caughtPokemons));
+      setCaughtPokemons(caughtPokemons);
+    }
+  };
+
+  const releasePokemon = (pokemonName) => {
+    const caughtPokemons = JSON.parse(localStorage.getItem('caughtPokemons')) || [];
+    const updatedCaughtPokemons = caughtPokemons.filter(p => p.name !== pokemonName);
+    localStorage.setItem('caughtPokemons', JSON.stringify(updatedCaughtPokemons));
+    setCaughtPokemons(updatedCaughtPokemons);
+  };
+
+  const switchToCaughtPage = () => {
+    setView('caught');
+  };
+
+  const switchToSearchPage = () => {
+    setView('search');
+  };
+
+  useEffect(() => {
+    if (view === 'search') {
+      const fetchPokemons = async () => {
+        try {
+          const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20');
+          const data = await response.json();
+
+          const pokemonDetails = await Promise.all(
+            data.results.map(async (pokemon) => {
+              const detailsResponse = await fetch(pokemon.url);
+              return await detailsResponse.json();
+            })
+          );
+
+          setPokemons(pokemonDetails);
+        } catch (error) {
+          console.error('Error fetching the Pokémon:', error);
+        }
+      };
+
+      fetchPokemons();
+    }
+  }, [view]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div>
+      <div className="flex m-8">
+        {view === 'search' && (
+          <>
+            <Input
+              className="w-1/2"
+              type="text"
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+              placeholder="Search for a Pokemon..."
             />
-          </a>
+            <Button className="mx-8" onClick={handleSearch}>
+              Search
+            </Button>
+          </>
+        )}
+        {view === 'caught' && (
+          <Button className="mx-8" onClick={switchToSearchPage}>
+            Back to Search
+          </Button>
+        )}
+        <Button className="mx-8" onClick={switchToCaughtPage}>
+          Caught Pokémons
+        </Button>
+      </div>
+      {view === 'search' ? (
+        <div>
+          {selectedPokemon ? (
+            <div className="bg-white shadow-md rounded-lg p-4">
+              <div className="flex justify-center">
+                <img src={selectedPokemon.sprites.front_default} alt={selectedPokemon.name} className="w-24 h-24 object-contain mb-4" />
+              </div>
+              <h3 className="text-lg font-bold mb-2 text-center">{selectedPokemon.name}</h3>
+              <p className="text-sm text-center">Weight: {selectedPokemon.weight}</p>
+              <p className="text-sm text-center">Height: {selectedPokemon.height}</p>
+              <p className="text-sm text-center">Types: {selectedPokemon.types.map(typeInfo => typeInfo.type.name).join(', ')}</p>
+              <p className="text-sm text-center">Stats:</p>
+              <ul className="text-sm text-center">
+                {selectedPokemon.stats.map(stat => (
+                  <li key={stat.stat.name}>{stat.stat.name}: {stat.base_stat}</li>
+                ))}
+              </ul>
+              {speciesData && (
+                <p className="text-sm text-center">
+                  Description: {
+                    speciesData.flavor_text_entries.find(entry => entry.language.name === 'en').flavor_text
+                  }
+                </p>
+              )}
+            </div>
+          ) : (
+            <div>
+              <h2 className="text-2xl font-bold mb-4">First 20 Pokémon:</h2>
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                {pokemons.map((pokemon) => (
+                  <div
+                    key={pokemon.name}
+                    className="bg-white shadow-md rounded-lg p-4 cursor-pointer"
+                    onClick={() => handleClick(pokemon.name)}
+                  >
+                    <div className="flex justify-center">
+                      <img src={pokemon.sprites.front_default} alt={pokemon.name} className="w-24 h-24 object-contain mb-4" />
+                    </div>
+                    <h3 className="text-lg font-bold mb-2 text-center">{pokemon.name}</h3>
+                    <p className="text-sm text-center">Weight: {pokemon.weight}</p>
+                    <p className="text-sm text-center">Height: {pokemon.height}</p>
+                    <p className="text-sm text-center">Types: {pokemon.types.map(typeInfo => typeInfo.type.name).join(', ')}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      ) : (
+        <div>
+          <h2 className="text-2xl font-bold mb-4">Caught Pokémon:</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            {caughtPokemons.map((pokemon) => (
+              <div key={pokemon.name} className="bg-white shadow-md rounded-lg p-4">
+                <div className="flex justify-center">
+                  <img src={pokemon.sprites.front_default} alt={pokemon.name} className="w-24 h-24 object-contain mb-4" />
+                </div>
+                <h3 className="text-lg font-bold mb-2 text-center">{pokemon.name}</h3>
+                <p className="text-sm text-center">Weight: {pokemon.weight}</p>
+                <p className="text-sm text-center">Height: {pokemon.height}</p>
+                <p className="text-sm text-center">Types: {pokemon.types.map(typeInfo => typeInfo.type.name).join(', ')}</p>
+                <Button className="mt-4" onClick={() => releasePokemon(pokemon.name)}>
+                  Release
+                </Button>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
   );
-}
+};
+
+export default PokemonApp;
